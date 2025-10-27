f049f2f2b3921a10e00c919f03192d8ed89f5166
mag162-ua <158452208+mag162-ua@users.noreply.github.com>
2025-10-27 11:33:06 +0100

    Add files via upload

--- ENGINE.bat
+++ ENGINE.bat
@@ -1,5 +1,5 @@
 TITLE ENGINE
 SET /P PUERTO_E="PUERTO del ENGINE : "
 
-docker exec -it p1-cp_engine-1 python ev_cp_engine.py kafka:9092 %PUERTO_E%
+docker exec -it ev-charging-cp_engine-1 python ev_cp_engine.py kafka:9092 %PUERTO_E%
 CMD /k
