7275a2a67848e86824c4089b78fb1f21152ae1de
afrp2-ua <158452135+afrp2-ua@users.noreply.github.com>
2025-10-31 13:30:38 +0100

    CENTRAL ACTUALIZADO

--- CENTRAL.bat
+++ CENTRAL.bat
@@ -1,4 +1,10 @@
 TITLE CENTRAL
+docker compose build --no-cache 
+docker compose push
 
+docker compose up -d zookeeper kafka
+docker ps
+docker compose run kafka-init
+docker compose up -d central
 docker exec -it ev-charging-central-1 python ev_central.py 5000 kafka:9092
 CMD /k
