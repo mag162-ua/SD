8f8f73ce63c3f491cfd0b2c3d9c2db66477d207d
mag162-ua <158452208+mag162-ua@users.noreply.github.com>
2025-11-25 19:23:17 +0100

    Delete MONITOR.bat

--- MONITOR.bat
+++
@@ -1,6 +0,0 @@
-TITLE MONITOR
-SET /P NUMERO_DADO="ID del CP : "
-SET /P IP_PUERTO_E="IP:PUERTO del ENGINE : "
-SET /P IP_PUERTO_C="IP_PUERTO de la CENTRAL: "
-docker exec -it ev-charging-cp_monitor-1 python ev_cp_monitor.py %IP_PUERTO_E% %IP_PUERTO_C% %NUMERO_DADO% 
-CMD /k
